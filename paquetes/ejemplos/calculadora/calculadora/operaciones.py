703728cc15ee9ab332c36dd90f0f2d45d2f7b7c9
RamirezAmayaS <s.ramirez34@uniandes.edu.co>
2018-06-29 13:24:23 -0500

    Se añade version final notebook Python

---
+++ paquetes/ejemplos/calculadora/calculadora/operaciones.py
@@ -0,0 +1,23 @@
+def divide(p,q):
+    if q == 0:
+        return "No es posible dividir por cero"
+    else:
+        return p/q
+
+def multiplica(p,q):
+    return p*q
+
+def resta(p,q):
+    return p-q
+
+def suma(p,q):
+    return p+q
+
+def saluda():
+    return 'Hola!'
+
+def despide():
+    return 'Adiós!'
+
+def pregunta():
+    return 'Qué más?'
---
+++ paquetes/ejemplos/calculadora/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='calculadora',
+      description='Calculadora de panaderia',
+      url='https://github.com/RamirezAmayaS/mlandpp-uniandes',
+      author='Simon Ramirez Amaya',
+      author_email='s.ramirez34@uniandes.edu.co',
+      license='MIT',
+      packages=['calculadora'],
+      install_requires=[],
+      zip_safe=False)
