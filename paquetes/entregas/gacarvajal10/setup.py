d77fda81d7d31243bee9df1bc9ddcfabe6661718
German Carvajal <ga.carvajal10@uniandes.edu.co>
2018-07-02 16:55:10 -0500

    Creación del paquete unsupervised requerido en el taller 1

---
+++ paquetes/entregas/gacarvajal10/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gacarvajal10',
+      description='Paquete k-medias taller 1 ML&PP',
+      url='https://github.com/german4589/mlandpp-uniandes',
+      author='German Carvajal',
+      author_email='ga.carvajal10@uniandes.edu.co',
+      license='MIT',
+      packages=['gacarvajal10'],
+      install_requires=['numpy'],
+      zip_safe=False)
