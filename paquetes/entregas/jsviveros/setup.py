fe5c186bd2c136521d77a9e803894fc64e50dde2
juansviveros <js.viveros@uniandes.edu.co>
2018-07-05 23:54:41 -0500

    Tarea KMeans

---
+++ paquetes/entregas/jsviveros/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='jsviveros',
+      description='Algoritmo Kmeans',
+      url='https://github.com/RamirezAmayaS/mlandpp-uniandes',
+      author='Juan Sebastian Viveros',
+      author_email='js.viveros@uniandes.edu.co',
+      license='MIT',
+      packages=['jsviveros'],
+      install_requires=[],
+      zip_safe=False)
