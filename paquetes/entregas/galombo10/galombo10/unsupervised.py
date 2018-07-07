bd806daf246237539c0bf063201b048a1b0624f3
Gabrielombo <40642906+Gabrielombo@users.noreply.github.com>
2018-07-06 21:21:16 -0500

    Add files via upload

---
+++ paquetes/entregas/galombo10/galombo10/unsupervised.py
@@ -0,0 +1,33 @@
+import numpy as np
+import math
+
+def centroide(k,data,cent):
+    n = np.array(data.shape)[0]
+    dataF = np.zeros(shape=(n,k),dtype=float)
+    for counter in range(0,k):
+        diff = cent[counter,:] - data
+        diff2 = diff**2
+        diffm = diff2[:,0] + diff2[:,1]
+        distance = [math.sqrt(x) for x in diffm]
+        dataF[:,counter] = distance
+    dataC = np.zeros(shape=(k,2),dtype=float)
+    indice = np.argmin(dataF,1)
+    for counter in range(0,k):
+        x = data[indice == counter][:,0].mean()
+        y = data[indice == counter][:,1].mean()
+        new_center = [x,y]
+        dataC[counter,:] = new_center
+    return(dataC)
+
+def kmeans(data,k):
+    center = np.random.rand(k,2)
+    while True:
+        new_center = centroide(k,data,center)
+        comp = (center - new_center).sum()
+        if comp == 0:
+            center = new_center
+            break
+        else:
+            center = new_center
+            new_center = centroide(k,data,center)
+    return center
---
+++ paquetes/entregas/galombo10/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='galombo10',
+      description='Método kmeans, Taller 1 - mlandpp',
+      url='https://github.com/Gabrielombo/mlandpp-uniandes',
+      author='Daniela Moncada, Santiago Perez, Sergio Julio, Gabriel Lombo',
+      author_email='ga.lombo10@uniandes.edu.co',
+      license='MIT',
+      packages=['galombo10'],
+      install_requires=['numpy','math'],
+      zip_safe=False)
