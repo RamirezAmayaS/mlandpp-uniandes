6e77299facd8cd6a822a58b939ef48df9ab1ec5e
Douglas Newball <d.newball10@uniandes.edu.co>
2018-07-07 18:24:44 -0500

    Se carga el paquete de K-means Taller 1 punto 2. Douglas Newball, Daniela Gualtero y Germán Orbegozo

---
+++ paquetes/entregas/dnewball10/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dnewball10',
+      description='Taller 1 MLandPP - Punto 2: K-means',
+      url='https://github.com/RamirezAmayaS/mlandpp-uniandes',
+      author='Douglas Newball Ramírez',
+      author_email='d.newball10@uniandes.edu.co',
+      license='MIT',
+      packages=['dnewball10'],
+      install_requires=['numpy'],
+      zip_safe=False)
