fb16a39a6ff52d43bf881c0f7c38ec68a5215136
Gabrielombo <40642906+Gabrielombo@users.noreply.github.com>
2018-07-06 21:32:58 -0500

    Update setup.py

--- paquetes/entregas/galombo10/setup.py
+++ paquetes/entregas/galombo10/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='galombo10',
       description='Método kmeans, Taller 1 - mlandpp',
       url='https://github.com/Gabrielombo/mlandpp-uniandes',
-      author='Daniela Moncada, Santiago Perez, Sergio Julio, Gabriel Lombo',
+      author='Daniela Moncada, Santiago Perez, Sergio Briceño, Gabriel Lombo',
       author_email='ga.lombo10@uniandes.edu.co',
       license='MIT',
       packages=['galombo10'],
