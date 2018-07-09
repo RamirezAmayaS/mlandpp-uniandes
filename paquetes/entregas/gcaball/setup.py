5be887b05236b858f9643d0391728a970ff72092
Gustavo Caballero <g-caball@uniandes.edu.co>
2018-07-09 07:47:41 -0500

    otra vez

---
+++ paquetes/entregas/gcaball/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gcaball',
+      description='Taller 1 - mlandpp-uniandes',
+      url='https://github.com/RamirezAmayaS/mlandpp-uniandes',
+      author='Gustavo Caballero <g-caball@uniandes.edu.co>, María Camila Lara <mc.lara796@uniandes.edu.co> ',
+      author_email='g-caball@uniandes.edu.co',
+      license='MIT',
+      packages=['gcaball'],
+      install_requires=['numpy','sklearn'],
+      zip_safe=False)
