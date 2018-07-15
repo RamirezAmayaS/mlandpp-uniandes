ce71ffbe887082da84e3e3bbd56b96853b6665d5
Alvaro Morales <alvaro0126@gmail.com>
2018-07-15 18:43:36 -0500

    Punto 3 Taller 1 - Corregido Se editan las lineas 17 y 24 para eliminar 4 espacios en cada una y se quita el punto del nombre de usuario

---
+++ paquetes/entregas/aamorales/aamorales/unsupervised.py
@@ -0,0 +1,37 @@
+import numpy as np
+#Programacion basada en la publicada en: https://datasciencelab.wordpress.com/2013/12/12/clustering-with-k-means-in-python/
+#Tambien se consulto: https://codereview.stackexchange.com/questions/80050/k-means-clustering-algorithm-in-python
+#y https://flothesof.github.io/k-means-numpy.html
+
+def cluster_points(puntos, centroide):
+    clusters  = {}
+    for x in puntos:
+        bestmukey = min([(i[0], np.linalg.norm(x-centroide[i[0]])) for i in enumerate(centroide)], key=lambda t:t[1])[0]
+
+        try:
+            clusters[bestmukey].append(x)
+        except KeyError:
+            clusters[bestmukey] = [x]
+    return clusters
+
+def reevaluate_centers(centroide, clusters):
+    centroide_nuevo = []
+    keys = sorted(clusters.keys())
+    for k in keys:
+        centroide_nuevo.append(np.mean(clusters[k], axis = 0))
+    return centroide_nuevo
+
+def centroide_final(centroide, centroide_viejo):
+    return (set([tuple(a) for a in centroide]) == set([tuple(a) for a in centroide_viejo]))
+
+    from numpy import random
+
+def kmeans(puntos, k):
+
+    centroide_viejo = np.random.randn(k, 2)
+    centroide = np.random.randn(k, 2)
+    while not  centroide_final(centroide, centroide_viejo):
+        centroide_viejo = centroide
+        clusters = cluster_points(puntos, centroide)
+        centroide = reevaluate_centers(centroide_viejo, clusters)
+    return centroide
---
+++ paquetes/entregas/aamorales/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='aamorales',
+      description='Entrega para handout 1 - mlandpp-uniandes',
+      url='https://github.com/alvaro0126/mlandpp-uniandes',
+      author='Alvaro Morales, Tatiana Martinez, Ana Diaz, Sebastian Naranjo',
+      author_email='aa.morales@uniandes.edu.co',
+      license='MIT',
+      packages=['aamorales'],
+      install_requires=['numpy','sklearn'],
+zip_safe=False)
